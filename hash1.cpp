1dfaa69378ba03eaf0ea4777e3e9f64a38483db6
subhash1e <85139394+subhash1e@users.noreply.github.com>
2021-06-01 06:04:06 +0530

    Add file 1st of 20

---
+++ hash1.cpp
@@ -0,0 +1,133 @@
+/*
+Description:
+
+Code for saving contacts with phone no. and Name
+hash using phone no.
+
+insert delete and search is done in constant time complexity
+
+*/
+
+#include <iostream>
+using namespace std;
+
+#define SIZE 29
+#define ll long long int 
+
+
+
+struct Contacts
+{
+	string Name;
+	ll Phone;
+	
+};
+
+struct Contacts* hashArray[SIZE];
+//hash function return index
+
+
+ll hashCode(ll Phone)
+{
+	return Phone%SIZE;
+}
+
+//search method
+
+struct Contacts *search(ll Phone){
+	//get hash 
+	ll hashindex = hashCode(Phone);
+
+	//move in array until an empty
+	while(hashArray[hashindex]!=NULL)
+	{
+		if (hashArray[hashindex]->Phone == Phone)
+				return hashArray[hashindex];
+		//else go to next cell
+			hashindex = (hashindex+1)%SIZE;
+			// wrap around the table
+	}
+
+
+// linear probing
+	return NULL;
+
+
+}
+
+
+// insert method
+
+void insert(ll Phone, string Name)
+{
+	ll hashindex = hashCode(Phone);
+	struct Contacts *Item  = (struct Contacts*)malloc(sizeof(struct Contacts));
+	Item->Name = Name;
+	Item->Phone = Phone;
+
+	//move in array until an empty or deleted cell
+
+	while(hashArray[hashindex] != NULL && hashArray[hashindex]->Phone != -1){
+
+		++hashindex;
+		hashindex%=SIZE;
+	}
+
+	hashArray[hashindex] = Item ;
+
+}
+
+// Delete Operation
+
+struct Contacts* deleteC(struct Contacts *Item)
+{
+	//get Phone
+	ll iPhone = Item->Phone;
+
+	struct Contacts *dummyItem  = (struct Contacts*)malloc(sizeof(struct Contacts));
+dummyItem->Phone = -1;
+
+
+	//get the hash
+	ll hashindex = hashCode(iPhone);
+
+	// move in array until an empty
+	while(hashArray[hashindex]!=NULL)
+	{
+		if (hashArray[hashindex]->Phone == iPhone)
+		{
+			struct Contacts* temp = hashArray[hashindex];
+			//assign it a dummy
+			hashArray[hashindex]  = dummyItem;
+			return temp;
+
+		}
+
+		//go and search for next cell
+		++hashindex;
+		hashindex%=SIZE;
+
+	}
+	return NULL;
+}
+
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+	
+	struct Contacts *x1;
+	insert(9521399493,"Subhash Kumar");
+	insert(9085719273,"Rahul Kumar");
+	insert(8294107695,"sk");
+
+	x1 = search(9521399493);
+	cout << x1->Name << endl;
+	x1  = search(8294107695);
+	cout << x1->Name << endl;
+
+	
+}
